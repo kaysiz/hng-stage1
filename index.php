fe63a6b9ae7f14524a2a588fb493cd28b1d1c0f6
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-04-13 15:53:45 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>HNG - Stage 1</title>
+</head>
+<body>
+    <main>
+        <p id="name">Kudakwashe Siziva</p>
+        <p id="time"><?= date('H:i:s');?></p>
+    </main>
+    <footer>
+        <p>&copy; Copyright <?= date('Y');?></p>
+    </footer>
+</body>
+</html>
